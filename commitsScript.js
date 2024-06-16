96baa0ac7199440d753ee38770911fdb1e4f68a4
Ramiro Grisales <ramiro.grisales@centrohub.co>
2024-06-15 20:43:58 -0500

    prueba2

--- commitsScript.js
+++ commitsScript.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process')
 
 const message = process.argv[2] || 'bugs fixed'
-const gitBranch = process.argv[2] || 'error'
+const gitBranch = process.argv[3] || 'error'
 
 const gitAdd = 'git add .'
 const gitCommit = `git commit -m"${message}"`
@@ -12,21 +12,21 @@ exec(gitAdd, (err, stdout, stderr) => {
     console.error(`hubo un error ${stderr}`)
     return
   }
-  console.log(`Ejecucion de add ${stdout}`)
+  console.log(`Cambios agregado correctamente: ${stdout}`)
 
   exec(gitCommit, (err, stdout, stderr) => {
     if (err) {
       console.error(`hubo un error enviando el commit ${stderr}`)
       return
     }
-    console.log(`Ejecucion del commit ${stdout}`)
+    console.log(`Commit "${message}" enviado correctamente ${stdout}`)
 
     exec(gitPush, (err, stdout, stderr) => {
       if (err) {
         console.error(`hubo un error en el push ${stderr}`)
         return
       }
-      console.log(`Ejecucion del push ${stdout}`)
+      console.log(`Push ejeutado correctamente a la rama ${gitBranch} ${stdout}`)
     })
   })
 })
